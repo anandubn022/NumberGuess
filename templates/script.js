70f25fe8e5783975bd17fd551cfc972736113d2a
anandubn022 <anandubn022@gmail.com>
2026-05-06 21:41:21 +0530

    js

--- templates/script.js
+++ templates/script.js
@@ -1,3 +1,6 @@
 async function guessSend() {
-    
+    const userGuess = document.getElementById("userGuess").value;
+    const response = await fetch('/guess', {method:POST, headers:{'Content-Type':'application/json'}, body:JSON.stringify({userGuess:userGuess})});
+    const data = await response.json();
+    document.getElementById("systemOutput").value = data.message;
 }
